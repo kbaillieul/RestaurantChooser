f19f5ec56573c776f63b018b770caf9118a2e928
Kristine Baillieul <kbaillieul1@gmail.com>
2021-09-17 16:26:21 -0230

    Added message for no selected restaurant

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ formSubmit.addEventListener("click", submitForm);
 
 function submitForm() {
   let restaurants = [
-    ["Piato", "pizza", "downtown", "moderate"],
-    ["Quesada", "mexican", "westEnd", "low"],
-    ["Basho", "sushi", "downtown", "high"],
-    ["Pi", "pizza", "downtown", "moderate"],
+    ["Piato, ", "pizza", "downtown", "moderate"],
+    ["Quesada, ", "mexican", "westEnd", "low"],
+    ["Basho, ", "sushi", "downtown", "high"],
+    ["Pi, ", "pizza", "downtown", "moderate"],
+    ["Acropolis, ", "pizza", "westEnd", "low"],
+    ["Cojones, ", "mexican", "downtown", "moderate"],
+    ["Sun Sushi (Torbay Road), ", "sushi", "eastEnd", "moderate"],
+    ["Sun Sushi (Mount Pearl), ", "sushi", "westEnd", "moderate"],
+    ["Sun Sushi (Downtown), ", "sushi", "downtown", "moderate"],
+    ["Quintanas, ", "mexican", "eastEnd", "moderate"],
   ];
   let foodTypeInput = document.getElementById("foodType");
   let foodTypeSelection = foodTypeInput.value;
@@ -20,7 +26,7 @@ function submitForm() {
     priceSelection,
   ];
   let options = [];
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 10; i++) {
     if (
       selectedOptions[0] === restaurants[i][1] &&
       selectedOptions[1] === restaurants[i][2] &&
@@ -29,5 +35,9 @@ function submitForm() {
       options += restaurants[i][0];
     }
   }
-  alert(options);
+  if (options.length === 0) {
+    alert("No restaurants meet your criteria");
+  } else {
+    alert(`The restaurants that meet your criteria are ${options}`);
+  }
 }
