610cd6d6ec210fa35b21013811d5525efda6fb95
Kristine Baillieul <kbaillieul1@gmail.com>
2021-11-02 16:14:33 -0230

    Fetch for Nearby Places function added

--- mapsapi.js
+++ mapsapi.js
@@ -6,3 +6,30 @@ function initMap() {
     zoom: 12,
   });
 }
+
+// var axios = require("axios");
+
+// var config = {
+//   method: "get",
+//   url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyDhxgRqOzJUo_L5_2hJoHdv9nd5luSvSHE",
+//   headers: {},
+// };
+
+// axios(config)
+//   .then(function (response) {
+//     console.log(JSON.stringify(response.data));
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+const api_url =
+  "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=47.560539%2C-52.71025&radius=1500&type=restaurant&key=key";
+
+async function getData() {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  console.log(data);
+}
+
+getData();
