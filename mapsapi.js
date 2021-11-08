656c5bafbfb308d4cce7ef556384edf40159c7f2
Kristine Baillieul <kbaillieul1@gmail.com>
2021-11-08 10:44:39 -0330

    Inclusion of places library for Maps API

--- mapsapi.js
+++ mapsapi.js
@@ -1,14 +1,8 @@
 "use strict";
 
-let requestSubmit = document.getElementById("apiSubmit");
-requestSubmit.addEventListener("click", getData);
-const api_url =
-  "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=47.560539%2C-52.71025&radius=1500&type=restaurant&key=key";
-// "https://jsonplaceholder.typicode.com/posts/1";
-
-async function getData() {
-  const response = await fetch(api_url);
-  const data = await response.json();
-  console.log(data);
-  document.getElementById("apiOutput").innerHTML = data.body;
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -33.866, lng: 151.196 },
+    zoom: 15,
+  });
 }
